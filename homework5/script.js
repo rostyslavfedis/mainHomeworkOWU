89b2fa75f57e9593f489ef1438aed001044ffe59
rostyslavfedis <rostyk110501@gmail.com>
2020-10-30 16:38:29 +0200

    hw

---
+++ homework5/script.js
@@ -0,0 +1,5 @@
+let array = ['<div>hello</div>','<div>what</div>','<div>is</div>','<div>your</div>','<div>name</div>','<div>and</div>','<div>what</div>','<div>is</div>','<div>your</div>','<div>age</div>'];
+
+ for (let i = 0; i<array.length; i++){
+     document.write(array[i]);
+ }
---
+++ homework6/script.js
@@ -0,0 +1,182 @@
+//task1
+// let num =[1,2,3,4,5];
+// console.log(num);
+//task2
+// let str=['one','two','tree','four','five'];
+// console.log(str);
+//task3
+// let aray = [1,'one',true, 'two',3 ]  ;
+// console.log(aray);
+
+//task4
+
+// let array=[];
+// array[0]="round";
+// array[1]="lomachenko"
+// array[2]="usuk"
+// array[3]="lomus"
+
+// console.log(array);
+//task5
+// let array = ['<div>one</div>','<div>two</div>','<div>three</div>','<div>four</div>','<div>five</div>','<div>six</div>','<div>seven</div>','<div>eight</div>','<div>nine</div>','<div>ten</div>'];
+
+//  for (let i = 0; i<array.length; i++){
+//      document.write(array[i]);
+//  }
+
+//task6
+// let array = ['<div>one</div>','<div>two</div>','<div>three</div>','<div>four</div>','<div>five</div>','<div>six</div>','<div>seven</div>','<div>eight</div>','<div>nine</div>','<div>ten</div>'];
+
+//  for (let i = 0; i<array.length; i++){
+//      document.write(array[i]);
+//      document.write([i]);
+    
+// }
+//task7
+//  const array=['<h1>lorem</h1>','<h1>ipsum</h1>','<h1>dolor</h1>','<h1>sit</h1>','<h1>amet</h1>','<h1>consectetur</h1>','<h1>adipiscing</h1>','<h1>elit</h1>','<h1>sed</h1>','<h1>do</h1>','<h1>eiusmod</h1>','<h1>tempor</h1>','<h1>incididunt</h1>','<h1>ut</h1>','<h1>labore</h1>','<h1>et</h1>','<h1>dolore</h1>','<h1>magna</h1>','<h1>aliqua</h1>','<h1>ut</h1>'];
+// let i=0;
+// while(i<array.length){
+//     document.write(array[i]);
+//     i++;
+// }
+
+// task8
+//  const array=['<h1>lorem</h1>','<h1>ipsum</h1>','<h1>dolor</h1>','<h1>sit</h1>','<h1>amet</h1>','<h1>consectetur</h1>','<h1>adipiscing</h1>','<h1>elit</h1>','<h1>sed</h1>','<h1>do</h1>','<h1>eiusmod</h1>','<h1>tempor</h1>','<h1>incididunt</h1>','<h1>ut</h1>','<h1>labore</h1>','<h1>et</h1>','<h1>dolore</h1>','<h1>magna</h1>','<h1>aliqua</h1>','<h1>ut</h1>'];
+// let i=0;
+// while(i<array.length){
+//     document.write(array[i]);
+//     document.write( [i] );
+//     i++;
+// }
+
+//task9
+// let arr=[2,4,1,7,5,3,2,1,9,4];
+// for(let i=0; i<arr.length;i++){
+// document.write(arr[i]);
+// }
+
+//task10
+// let arr=['JS ','is ','cool ','axaxax'];
+// for(let i=0; i<arr.length;i++){
+// document.write(arr[i]);
+// }
+
+//task11
+// let arr=['JS ',1 ,true,' axaxax', 228];
+// for(let i=0; i<arr.length;i++){
+// document.write(arr[i]);
+// }  
+
+//task12
+//  let anArray=['if ',true,false,212,13,'11',true,'4123',false,12];
+//  for (i = 0; i < anArray.length; i++) {
+//         if (typeof (anArray[i]) == 'boolean') {
+//             console.log(anArray[i]);
+//         }
+//     }
+
+//task13
+// let anArray=['if ',true,false,212,13,'11',true,'4123',false,12];
+// for (i = 0; i < anArray.length; i++) {
+//        if (typeof (anArray[i]) == 'number') {
+//            console.log(anArray[i]);
+//        }
+//    }
+ //task14
+//  let anArray=['if ',true,false,212,13,'11',true,'4123',false,12];
+//  for (i = 0; i < anArray.length; i++) {
+//         if (typeof (anArray[i]) == 'string') {
+//             console.log(anArray[i]);
+//         }
+//     }
+
+//task15
+// let arrNew=[];
+// arrNew[0]=1;
+// arrNew[1]='re';
+// arrNew[2]=true;
+// arrNew[3]=22;
+// arrNew[4]='rere';
+// arrNew[5]=false;
+// arrNew[6]='qqqqq';
+// arrNew[7]=445444;
+// arrNew[8]='true';
+// arrNew[9]='end';
+// for(let i=0;i<arrNew.length;i++){
+//     console.log(arrNew[i]);
+// }
+
+//task16
+// for(let i=0;i<=10;i++){
+//     console.log(i);   
+//     document.write(i);
+// }
+
+//task17
+// for(let i=0;i<=100;i=i+2){
+//         console.log(i);   
+//         document.write(i);
+//     }
+
+//task18
+// for(let i=0;i<=100;i++){
+//     if(i%2===0){
+//       console.log(i);   
+//         document.write(i);  
+//     }
+        
+//     }
+
+//task19
+// for(let i=0;i<=100;i++){
+//     if(i%2){
+//       console.log(i);   
+//         document.write(i);  
+//     }
+        
+//     }
+
+//  CLOCK 2хв
+// for(let x=0; x<=59; x++){
+//     console.log(x+'ceк');
+//    if(x==59){
+//     for(let x=0; x<=59; x++)
+//     console.log('1 хв:'+x+'сек');
+//    }
+//    }
+//  CLOCK 2год 20хв
+
+    //    for(let x=0; x<=2; x++){
+    //     console.log(x+'год');
+    //     if (x==0){
+    //         for(let y=0; y<=59; y++){
+    //             console.log(y+'хв')
+    //         }
+    //     }else if(x==1){
+    //         for(let y=0; y<=59; y++){
+    //             console.log(x+'год'+y+'хв')
+    //         }
+    //     }else{
+    //         for(let y=0; y<=20; y++){
+    //             console.log(x+'год'+y+'хв')
+    //           }
+    //     }
+    //    }
+
+    //dodatkovo1
+    // let abc=[ 'a', 'b', 'c'];
+    // let str="";
+
+    // for(i=0; i<abc.length; i++){
+    //    str+=abc[i];
+    // }
+    // console.log(str);
+ //2
+    let word=['a','b','c'];
+    let str='';
+    let i=0;
+    while(i<word.length){
+        str+=word[i];
+        i++;
+           }
+console.log(str);
