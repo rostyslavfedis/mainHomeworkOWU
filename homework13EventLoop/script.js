6ae2e59797cc33747bd8fff626a172afd9cc0ed2
rostyslavfedis <rostyk110501@gmail.com>
2020-11-20 14:59:34 +0200

    async await

--- homework13EventLoop/script.js
+++ homework13EventLoop/script.js
@@ -184,117 +184,117 @@
 //==========================================================================================================
 
 
-function startMyRoutine(){
-return new Promise((resolve,reject)=>{
-        setTimeout(()=>{
-            resolve('Start my routine!')
-        },2000);
-    })
-}
+// function startMyRoutine(){
+// return new Promise((resolve,reject)=>{
+//         setTimeout(()=>{
+//             resolve('Start my routine!')
+//         },2000);
+//     })
+// }
 
-function listOfRoutine0(time){
-return new Promise((resolve,reject)=>{
-    setTimeout(()=>{
-        if(time>9){
-            reject('You should go to bed faster! '+'You overslept!');
+// function listOfRoutine0(time){
+// return new Promise((resolve,reject)=>{
+//     setTimeout(()=>{
+//         if(time>9){
+//             reject('You should go to bed faster! '+'You overslept!');
         
-        }
-        resolve('1.Прокидаюсь.')
-    },1000);
-  })
-}
-function listOfRoutine1(water){
-return new Promise((resolve,reject)=>{
-    setTimeout(()=>{
-        if(water<=0){
-            reject('NOT WATER!');
-}
-        resolve('2.Вмиваюсь.')
+//         }
+//         resolve('1.Прокидаюсь.')
+//     },1000);
+//   })
+// }
+// function listOfRoutine1(water){
+// return new Promise((resolve,reject)=>{
+//     setTimeout(()=>{
+//         if(water<=0){
+//             reject('NOT WATER!');
+// }
+//         resolve('2.Вмиваюсь.')
 
-},1000);
-})
-}
-function listOfRoutine2(meat){
-return new Promise((resolve,reject)=>{
-    setTimeout(()=>{
-        if(meat<=0){
-            reject('NOT MEAT!')
-        }
-        resolve('3.Снідаю.');
-},1000);
-})
-}
-function listOfRoutine3(internet){
-return new Promise((resolve,reject)=>{
-    setTimeout(()=>{
-            if(internet<=0){
-                reject('NOT INTERNET!')
-            }
-            resolve('4.Предивляюсь лекцію))');
-    },1000);
-})
-}
-function listOfRoutine4(meat){
-return new Promise((resolve,reject)=>{
-        setTimeout(()=>{
-            if(meat<=0){
-                reject('NOT MEAT!')
-            }
-            resolve('5.Обідаю.');
-    },1000);
-})
-}
-function listOfRoutine5(light){
-return new Promise((resolve,reject)=>{
-        setTimeout(()=>{
-            if(light<=0){
-                reject('NO LIGHT!')
-            }
-            resolve('6.Виконую д/з.');
-    },1000);
-})
-}
-function listOfRoutine6(meat){
-return new Promise((resolve,reject)=>{
+// },1000);
+// })
+// }
+// function listOfRoutine2(meat){
+// return new Promise((resolve,reject)=>{
+//     setTimeout(()=>{
+//         if(meat<=0){
+//             reject('NOT MEAT!')
+//         }
+//         resolve('3.Снідаю.');
+// },1000);
+// })
+// }
+// function listOfRoutine3(internet){
+// return new Promise((resolve,reject)=>{
+//     setTimeout(()=>{
+//             if(internet<=0){
+//                 reject('NOT INTERNET!')
+//             }
+//             resolve('4.Предивляюсь лекцію))');
+//     },1000);
+// })
+// }
+// function listOfRoutine4(meat){
+// return new Promise((resolve,reject)=>{
+//         setTimeout(()=>{
+//             if(meat<=0){
+//                 reject('NOT MEAT!')
+//             }
+//             resolve('5.Обідаю.');
+//     },1000);
+// })
+// }
+// function listOfRoutine5(light){
+// return new Promise((resolve,reject)=>{
+//         setTimeout(()=>{
+//             if(light<=0){
+//                 reject('NO LIGHT!')
+//             }
+//             resolve('6.Виконую д/з.');
+//     },1000);
+// })
+// }
+// function listOfRoutine6(meat){
+// return new Promise((resolve,reject)=>{
 
-        setTimeout(()=>{
-            if(meat<=0){
-                reject('NOT MEAT!')
-            }
-            resolve('7.Вечеряю.');
-    },1000);
-})
-}
-function listOfRoutine7(internet){
-return new Promise((resolve,reject)=>{
-        setTimeout(()=>{
-            if(internet<=0){
-                reject('NOT INTERNET!');
-            }
-            resolve('8.Дивлюсь нову лекцію.');
-    },1000);
-})
-}
-function listOfRoutine8(wather){
-return new Promise((resolve,reject)=>{
-         setTimeout(()=>{
-            if(wather<=0){
-                reject('WATHER IS BAD!');
-            }
-            resolve('9.Гуляю.');
-    },1000);
-})
-}
-function listOfRoutine9(bad){
-return new Promise((resolve,reject)=>{
-        setTimeout(()=>{
-            if(bad<=0){
-                reject('YOU DIDN`T! HAVE BED');
-            }
-            resolve('10.Лягаю спати.');
-    },1000);
-})
-}
+//         setTimeout(()=>{
+//             if(meat<=0){
+//                 reject('NOT MEAT!')
+//             }
+//             resolve('7.Вечеряю.');
+//     },1000);
+// })
+// }
+// function listOfRoutine7(internet){
+// return new Promise((resolve,reject)=>{
+//         setTimeout(()=>{
+//             if(internet<=0){
+//                 reject('NOT INTERNET!');
+//             }
+//             resolve('8.Дивлюсь нову лекцію.');
+//     },1000);
+// })
+// }
+// function listOfRoutine8(wather){
+// return new Promise((resolve,reject)=>{
+//          setTimeout(()=>{
+//             if(wather<=0){
+//                 reject('WATHER IS BAD!');
+//             }
+//             resolve('9.Гуляю.');
+//     },1000);
+// })
+// }
+// function listOfRoutine9(bad){
+// return new Promise((resolve,reject)=>{
+//         setTimeout(()=>{
+//             if(bad<=0){
+//                 reject('YOU DIDN`T! HAVE BED');
+//             }
+//             resolve('10.Лягаю спати.');
+//     },1000);
+// })
+// }
     
 
 
@@ -305,58 +305,216 @@ return new Promise((resolve,reject)=>{
 
 
 
-startMyRoutine(9)
-.then((value) =>{
-    console.log(value)
-    return listOfRoutine0(1);
-})
-.then((value) =>{
-    console.log(value)
-    return listOfRoutine1(1);
-})
-.then((value) =>{
-    console.log(value)
-    return listOfRoutine2(1);
-})
-.then((value) =>{
-    console.log(value)
-    return listOfRoutine3(1);
-})
-.then((value) =>{
-    console.log(value)
-    return listOfRoutine4(1);
-})
-.then((value) =>{
-    console.log(value)
-    return listOfRoutine5(0);
-})
-.then((value) =>{
-    console.log(value)
-    return listOfRoutine6(1);
-})
-.then((value) =>{
-    console.log(value)
-    return listOfRoutine7(1);
-})
-.then((value) =>{
-    console.log(value)
-    return listOfRoutine8(1);
-})
-.then((value) =>{
-    console.log(value)
-    return listOfRoutine9(1);
-})
-.then((value) =>{
-    console.log(value)
-})
-.catch(err=>{
-    console.log(err);
-})
+// startMyRoutine()
+// .then((value) =>{
+//     console.log(value)
+//     return listOfRoutine0(9);
+// })
+// .then((value) =>{
+//     console.log(value)
+//     return listOfRoutine1(1);
+// })
+// .then((value) =>{
+//     console.log(value)
+//     return listOfRoutine2(1);
+// })
+// .then((value) =>{
+//     console.log(value)
+//     return listOfRoutine3(1);
+// })
+// .then((value) =>{
+//     console.log(value)
+//     return listOfRoutine4(1);
+// })
+// .then((value) =>{
+//     console.log(value)
+//     return listOfRoutine5(0);
+// })
+// .then((value) =>{
+//     console.log(value)
+//     return listOfRoutine6(1);
+// })
+// .then((value) =>{
+//     console.log(value)
+//     return listOfRoutine7(1);
+// })
+// .then((value) =>{
+//     console.log(value)
+//     return listOfRoutine8(1);
+// })
+// .then((value) =>{
+//     console.log(value)
+//     return listOfRoutine9(1);
+// })
+// .then((value) =>{
+//     console.log(value)
+// })
+// .catch(err=>{
+//     console.log(err);
+// })
+//==========================================================================================================
   
+async function startMyRoutine(){
+let promise= new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            resolve('Start my routine!')
+        },2000);
+    })
+
+        let result = await promise;
+        console.log(result);
+    
+}
 
 
+async function listOfRoutine0(time){
+let promise= new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        if(time>9){
+            reject('You should go to bed faster! '+'You overslept!');
+        
+        }
+        resolve('1.Прокидаюсь.')
+    },1000);
+  })
+let result = await promise;
+    console.log(result);
+}
+
+async function listOfRoutine1(water){
+let promise= new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        if(water<=0){
+            reject('NOT WATER!');
+}
+        resolve('2.Вмиваюсь.')
+
+},1000);
+})
+let result = await promise;
+    console.log(result);
+}
+async function listOfRoutine2(meat){
+    let promise= new Promise((resolve,reject)=>{
+
+    setTimeout(()=>{
+        if(meat<=0){
+            reject('NOT MEAT!')
+        }
+        resolve('3.Снідаю.');
+},1000);
+})
+let result = await promise;
+    console.log(result);
+}
+async function listOfRoutine3(internet){
+    let promise= new Promise((resolve,reject)=>{
+
+    setTimeout(()=>{
+            if(internet<=0){
+                reject('NOT INTERNET!')
+            }
+            resolve('4.Предивляюсь лекцію))');
+    },1000);
+})
+let result = await promise;
+    console.log(result);
+}
+async function listOfRoutine4(meat){
+    let promise= new Promise((resolve,reject)=>{
+
+        setTimeout(()=>{
+            if(meat<=0){
+                reject('NOT MEAT!')
+            }
+            resolve('5.Обідаю.');
+    },1000);
+})
+let result = await promise;
+    console.log(result);
+}
+async function listOfRoutine5(light){
+    let promise= new Promise((resolve,reject)=>{
+
+        setTimeout(()=>{
+            if(light<=0){
+                reject('NO LIGHT!')
+            }
+            resolve('6.Виконую д/з.');
+    },1000);
+})
+let result = await promise;
+    console.log(result);
+}
+async function listOfRoutine6(meat){
+    let promise= new Promise((resolve,reject)=>{
 
 
+        setTimeout(()=>{
+            if(meat<=0){
+                reject('NOT MEAT!')
+            }
+            resolve('7.Вечеряю.');
+    },1000);
+})
+let result = await promise;
+    console.log(result);
+}
+async function listOfRoutine7(internet){
+    let promise= new Promise((resolve,reject)=>{
+
+        setTimeout(()=>{
+            if(internet<=0){
+                reject('NOT INTERNET!');
+            }
+            resolve('8.Дивлюсь нову лекцію.');
+    },1000);
+})
+let result = await promise;
+    console.log(result);
+}
+async function listOfRoutine8(wather){
+    let promise= new Promise((resolve,reject)=>{
+
+         setTimeout(()=>{
+            if(wather<=0){
+                reject('WATHER IS BAD!');
+            }
+            resolve('9.Гуляю.');
+    },1000);
+})
+let result = await promise;
+    console.log(result);
+}
+async function listOfRoutine9(bad){
+    let promise= new Promise((resolve,reject)=>{
+
+        setTimeout(()=>{
+            if(bad<=0){
+                reject('YOU DIDN`T! HAVE BED');
+            }
+            resolve('10.Лягаю спати.');
+    },1000);
+})
+let result = await promise;
+    console.log(result);
+}
+
+async function sunc(){
+    let res=await startMyRoutine();
+    let res1=await listOfRoutine0(9);
+    let res2=await listOfRoutine1(1);
+    let res3=await listOfRoutine2(1);
+    let res4=await listOfRoutine3(1);
+    let res5=await listOfRoutine4(1);
+    let res6=await listOfRoutine5(1);
+    let res7=await listOfRoutine6(1);
+    let res8=await listOfRoutine7(1);
+    let res9=await listOfRoutine8(1);
+    let res10=await listOfRoutine9(1);
+}
+
+sunc();
 
 
 
